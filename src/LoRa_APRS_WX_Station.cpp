38bf101eba77b7134fe61ac91aabe4535edc7044
richonguzman <richon2001@hotmail.com>
2024-03-04 16:45:43 -0300

    4 spaces mod

--- src/LoRa_APRS_WX_Station.cpp
+++ src/LoRa_APRS_WX_Station.cpp
@@ -11,31 +11,31 @@ TinyGPSPlus     gps;
 int contador = 0;
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED, OUTPUT);
-  delay(4000);
-  LoRa_Utils::setup();
-  GPS_Utils::setup();
+    Serial.begin(115200);
+    pinMode(LED, OUTPUT);
+    delay(4000);
+    LoRa_Utils::setup();
+    GPS_Utils::setup();
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  Serial.println("LED is on");
-  delay(1000);
-  digitalWrite(LED, LOW);
-  Serial.println("LED is off");
-  delay(1000);
+    digitalWrite(LED, HIGH);
+    Serial.println("LED is on");
+    delay(1000);
+    digitalWrite(LED, LOW);
+    Serial.println("LED is off");
+    delay(1000);
 
-  /*contador++;
-  String packet = "AB1CDE-7>APLRT1,WIDE1-1::CA2RXU-7 :testMessage " + String(contador);
-  LoRa_Utils::sendNewPacket(packet);
-  delay(15000);*/
+    /*contador++;
+    String packet = "AB1CDE-7>APLRT1,WIDE1-1::CA2RXU-7 :testMessage " + String(contador);
+    LoRa_Utils::sendNewPacket(packet);
+    delay(15000);*/
 
-  GPS_Utils::getData();
-  GPS_Utils::setDateFromData();
+    GPS_Utils::getData();
+    GPS_Utils::setDateFromData();
 
-  const auto time_now = now();
-  Serial.println(Utils::createDateString(time_now) + "   " + Utils::createTimeString(time_now));
+    const auto time_now = now();
+    Serial.println(Utils::createDateString(time_now) + "   " + Utils::createTimeString(time_now));
 
   
 
